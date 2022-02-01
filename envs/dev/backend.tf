4dc085724ebc9ba8fdc92d0b692c1610a1cee230
MasayaTahara <mocoso.0u1@gmail.com>
2022-02-01 22:47:00 +0900

    Fix backend

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,8 +1,12 @@
-terraform {
-    backend "s3" {
-        bucket         = "dev-devsecops-demo-tfstate-management-bucket"
-        key            = "terraform.tfstate"
-        region         = "ap-northeast-1"
-        dynamodb_table = "dev-devsecops-demo-tfstate-management-table"
-    }
-}
+# tfstate file is recommended to be managed by S3 and DynamoDB.
+# However backend setting depends on also the scale of what will be created by Terraform.
+# Therefore, backend setting is removed in this repository.
+
+# terraform {
+#     backend "s3" {
+#         bucket         = ""
+#         key            = ""
+#         region         = ""
+#         dynamodb_table = ""
+#     }
+# }
--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,11 +1,10 @@
-module "tfstate_manager_module" {
-    source = "github.com/MasayaTahara/tfstate-manager"
-    env    = "dev"
-    app    = "devsecops-demo"
-}
-
 module "code_repository" {
   source                 = "../../modules/codecommit"
   repository_name        = "devsecops-demo-app"
   repository_description = "This is the Sample App Repository for DevSecOps"
+}
+
+module "s3_web" {
+  source      = "../../modules/s3-web"
+  bucket_name = "devsecops-demo-website"
 }
