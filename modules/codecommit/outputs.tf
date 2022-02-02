a45f0ba6adfdc58f280e16919337c9e7f6e6f221
MasayaTahara <mocoso.0u1@gmail.com>
2022-02-02 22:05:53 +0900

    Remove CodeCommit

--- modules/codecommit/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repository_arn" {
-  value = aws_codecommit_repository.demo.arn
-  description = "Code Repository ARN"
-}
-
-output "repository_name" {
-  value = aws_codecommit_repository.demo.repository_name
-  description = "Code Repository Name"
-}
--- modules/codecommit/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "repository_name" {
-  type        = string
-  description = "Repository name"
-}
-
-variable "repository_description" {
-  type        = string
-  description = "Repository description"
-}
